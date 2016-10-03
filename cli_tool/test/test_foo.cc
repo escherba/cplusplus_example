249a6f94b9833cc1f91745f7c9f1e9bc00453b5c
Eugene Scherba <escherba@gmail.com>
2016-10-03 02:34:01 -0700

    tests pass

--- cli_tool/test/test_foo.cc
+++ cli_tool/test/test_foo.cc
@@ -6,6 +6,6 @@ TEST(foo_test, mean_variance)
     basic_stats stats;
     sample_accumulate(&stats);
 
-    EXPECT_EQ(stats.mean, 10.0);
-    EXPECT_EQ(stats.variance, 1.25);
+    ASSERT_DOUBLE_EQ(stats.mean, 10.0);
+    ASSERT_DOUBLE_EQ(stats.variance, 1.25);
 }
