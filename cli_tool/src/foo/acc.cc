52de719147960072806a466ab6e5adfed56a7c2b
Eugene Scherba <escherba@gmail.com>
2016-10-03 08:38:12 -0700

    describe doc target

--- cli_tool/src/foo/acc.cc
+++ cli_tool/src/foo/acc.cc
@@ -5,6 +5,11 @@
 
 using namespace boost::accumulators;
 
+/**
+ * @brief Demonstrates the use of Boost accumulators
+ *
+ * @param stats - a struct consisting of two doubles
+ */
 void sample_accumulate(basic_stats *stats) {
     accumulator_set<double, features<tag::mean, tag::variance>, int> acc;
     acc(8, weight = 1);
